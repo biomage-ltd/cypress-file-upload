032764ad0fe972a57661662c4aa1221afae06959
Martin Fosco <martinfosco@Martins-MacBook-Pro.local>
2021-09-09 15:20:14 -0300

    Add support for folder upload simulation

--- src/attachFile.js
+++ src/attachFile.js
@@ -6,11 +6,12 @@ import { resolveFile } from '../lib/file';
 import { merge } from '../lib/object';
 
 export default function attachFile(subject, fixtureOrFixtureArray, processingOptions) {
-  const { subjectType, force, allowEmpty } = merge(processingOptions, DEFAULT_PROCESSING_OPTIONS);
+  const { subjectType, force, allowEmpty, simulatedFilePath } = merge(processingOptions, DEFAULT_PROCESSING_OPTIONS);
   validateOptions({
     subjectType,
     force,
     allowEmpty,
+    simulatedFilePath,
   });
 
   const fixturesArray = Array.isArray(fixtureOrFixtureArray) ? fixtureOrFixtureArray : [fixtureOrFixtureArray];
@@ -19,7 +20,7 @@ export default function attachFile(subject, fixtureOrFixtureArray, processingOpt
   Cypress.cy.window({ log: false }).then(window => {
     const forceValue = force || getForceValue(subject);
 
-    Cypress.Promise.all(fixtures.map(f => resolveFile(f, window))) // resolve files
+    Cypress.Promise.all(fixtures.map((f, index) => resolveFile(f, window, simulatedFilePath[index]))) // resolve files
       .then(files => files.filter(f => validateFile(f, allowEmpty))) // error if any of the file contents are invalid
       .then(files => {
         attachFileToElement(subject, {
